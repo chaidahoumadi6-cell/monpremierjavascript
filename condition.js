59d108135cb767d67f541f1e86801e3a8e5995c1
chaidahoumadi6-cell <chaidahoumadi6@gmail.com>
2025-12-09 16:14:34 +0300

    j'ai fait les condition en javascript

---
+++ condition.js
@@ -0,0 +1,40 @@
+//les condition en javascript
+
+//j'affiche l'alerte SI le nombre de fruit est égal à 18
+let nombreFruit = 18;
+
+//Ma condition commence par le mot clé IF 
+if (nombreFruit == 18){//nous avons une comparaison à égalite stricte 
+    alert("vous avez atteint 18 fruit.");//fonction alert()
+
+};
+
+let noteEleve = 10
+
+if(noteEleve >= 10){
+    alert("vous etes admis avec une note de" + noteEleve);
+};
+
+
+let age4 = 18;
+
+//si l'age est égal ou superieur à 18
+if (age4 >= 18){
+    alert("vous pourez regarde rombo" + age4);
+};
+
+// si l'age est moins de 18
+if(age4<18){
+    alert("vous pourez regarde le roilion ");
+};
+
+let pointPermis = 12;
+ if(pointPermis == 12){// si le point permis est strictement égal à 12
+    alert("vous avez un bonus!");
+
+ } else{//sinon
+    alert("vous avez des malus!")
+ }
+
+
+
