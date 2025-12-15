701028ce1aae2038b1dc55e7ccf7341f4a9d6f21
chaidahoumadi6-cel <chaidahoumadi6@gmail.com>
2025-12-15 16:47:35 +0300

    j'ai récupère les élément situe dansle fichier HTML

--- dom_calcul_age.js
+++ dom_calcul_age.js
@@ -1,14 +1,37 @@
 
  
  // Vérifie que l'année saisie est valide
-if(anneeNaissance > 2005 & anneeNaissance <= anneeActuelle) {
+ /*
+if(anneeNaissance 2005  anneeNaissance <= anneeActuelle) {
     let age = anneeActuelle - anneeNaissance;
     ageAffiche = "age:" + age + "ans";
 
-};
-else{
-    ageAffiche ="age";
-};
+};*/
 
 
-console.log("age :",age );
+//je récupère un élement situé dans le fichier HTML
+ let elementBoutonValide = document.getElementById("boutonValide");
+
+ //La variable elementBoutonValide contient maintenant le bouton HTML
+ console.log(document.getElementById("nom") );
+
+//j'affiche l'élément récupère dans la console
+ console.log( elementBoutonValide.clientHeights);
+
+ let elementInputNom = document.querySelector("#nom")
+
+ console.log(document.head);
+ console.log(document.title);
+ console.log(document.body);
+ 
+ //je récupère l' élement HTML dont l'identifiant est recensement
+ let elementDivRecensement = document.querySelector("#recensement");
+ console.log(elementDivRecensement);
+ 
+ let elementP = document.querySelector("#paragraphe1");
+ console.log(elementP);
+
+ elementBoutonValide.addEventListener("click",function () {
+console.log("elementboutonValider:", elementBoutonValide);
+ });
+
