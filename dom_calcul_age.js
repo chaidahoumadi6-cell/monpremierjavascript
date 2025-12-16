f9ba275c5a4c72bc1c6bd9c0dfa2c33fe2d88c97
chaidahoumadi6-cel <chaidahoumadi6@gmail.com>
2025-12-16 16:42:04 +0300

    j' insére des balise dans le fichier  HTML grace au code javascript

--- dom_calcul_age.js
+++ dom_calcul_age.js
@@ -56,8 +56,58 @@ for (let i = 0; i < elementClassCommunes.length ; i++) {
 
 
     console.log(elementClassCommunes[i]);
-    
+
 
    // l'intérieur d'une boucle, elle ne sera visible qu'à l'intérieur de la boucle.
     console.log(elementClassCommunes[i].textContent);
 }
+
+
+
+//j' insère des balises dans le fichier HTML grace au code javascript
+
+//je crée un élement dans le (balise) nomé <section>
+let elementSection = document.createElement("section");
+
+//je récupère la balise <body> puis je stocke <body> dans la variable elementBody
+let elementBody = document.querySelector("body");
+
+//Ajoute cet élément HTML comme enfant à l’intérieur de celui-ci. 
+elementBody.appendChild(elementSection);
+
+let nouveauTitre = document.createElement("h3");
+
+nouveauTitre.textContent = "Mon titre de section";
+
+
+//Elle ajoute elementSection à la fin du <body> de la page.
+elementSection.appendChild(nouveauTitre);
+
+
+let elementMaman = document.createElement("Maman");
+
+elementBody.appendChild(elementMaman);
+
+let nouveauTitre1 = document.createElement("h3");
+nouveauTitre1.textContent ="Bonjour Maman";
+
+//créer la balise IMG
+let elementimage = document.createElement("img");
+
+//Donner le chemin de l'image
+elementimage.src = "fruitss.jpg";
+
+//
+elementimage.setAttribute("alt","Mon fruite");
+
+elementimage.className ="image-fruits";
+
+elementimage.classList.add("image","image2");
+
+//supprimer une classe de l' élément img
+elementimage.classList.remove("image2");
+
+
+//Ajouter l'image dans le body
+elementBody.appendChild(elementimage);
+
