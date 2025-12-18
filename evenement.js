1b002dff9e333fc4cc49194294c510c39a4c3b5c
chaidahoumadi6-cel <chaidahoumadi6@gmail.com>
2025-12-18 11:37:46 +0300

    j'ai recoperer les données saisies sur le formulaire

--- evenement.js
+++ evenement.js
@@ -2,20 +2,25 @@
 
 const elementboutonValiderDate = document.querySelector("#validerDate");
 
+
  // je déclare un objet date en utilisant la classe date ()
 const maDate = new Date();
 
-//
+
+// Sélectionne l'élément HTML ayant l'id "dateDuJour" et le stocke dans une variable
 let elementSpanDateDuJour = document.querySelector("#dateDuJour");
 
+
 let elementSpanDateDuJourFormat2 = document.querySelector("#dateDuJourFormat2");
+
 
 //mon événement "click"
 elementboutonValiderDate.addEventListener("click", function () {
 
+
+// Affiche un message dans la console pour indiquer que le bouton "valider date" a été cliqué
     console.log("### Je suis dans le bouton valider date ###");
 
-   
 
     //Je décris l' action à réaliser lorsque je clique sur le bouton valider 
     elementSpanDateDuJour.textContent = maDate;
@@ -29,12 +34,17 @@ elementboutonValiderDate.addEventListener("click", function () {
 //événement "keydown"
 let elementBoutonFonctionFleche = document.querySelector("#boutonFleche");
 
+
 //2.J'assosie l'évenement KEYDOWN mon élément HTML en utilisant la méthode addEventListener().
 // A l'intérieur de la méthod eAddEvent listener (), je passe une fonction flechée comme suit '() => {}
 elementBoutonFonctionFleche.addEventListener("keydown",(event) => {
 
+
+    // Affiche un message dans la console pour indiquer que l'événement "keydown" a été déclenché
     console.log("=== Je suis dans l'événement KEYDOWN ===");
 
+
+    // Affiche dans la console la touche du clavier pressée lors de l'événement
     console.log("clé event:", event.key);
 });
 
@@ -47,22 +57,55 @@ elementBoutonFonctionFleche.addEventListener("keydown",(event) => {
 //1.Tout d'abord, je récupère l' élement HTML que sera associé à l'évenement CHANGE
 let elementInputNom = document.querySelector("#nom");
 
+
 //2.J'assosie l'évenement CHANGE mon élément HTML en utilisant la méthode addEventListener().
 // A l'intérieur de la méthod eAddEvent listener (), je passe une fonction flechée comme suit '() => {}
+
 
 //Je dclare une variable
 let messageSalutation;
 
+
+// Sélectionne l'élément HTML ayant l'id "nomUtilisateur" et le stocke dans une variable
 let elementSpanNomUtilisateur = document.querySelector("#nomUtilisateur");
 
+
+// Ajoute un écouteur d'événement "change" sur le champ de saisie du nom
 elementInputNom.addEventListener("change", (event) => {
+
 
     console.log(event.target.value);
 
+    // Définit le message de salutation en utilisant la valeur saisie par l'utilisateur
     messageSalutation = `Bonjour ${event.target.value}`;
 
+    // Met à jour le contenu texte du span avec le message de salutation
     elementSpanNomUtilisateur.textContent = messageSalutation;
     
 
 });
+/**
+ * Envoyer le formulaire
+ * je vais récupérer les données saisies sur le formulaire:nom,prénom,situation,date de naissance
+ */
 
+//je récupérer la balise <form> avec ses enfants;Ensuite,je stocke la balise dans la variable elementForm
+let elementForm = document.querySelector("form");
+console.log("elementForm:", elementForm);
+
+//J'associe l'evénement 'submit'
+elementForm.addEventListener("submit",(event) =>{
+    event.preventDefault();
+    console.log("### Je suis évenement submit du formuliare###");
+
+    const nom = document.querySelector("#nom").value;
+    console.log("Nom:",nom);
+
+    const prenom = document.querySelector("#prenom").value;
+    console.log("prenom:",prenom);
+
+    const situation = document.querySelector("#situation").value;
+    
+
+
+});
